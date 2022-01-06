14329a6beb6c98180ceeedc5f323773a414c71a9
Radek <radek.paradecki@gmail.com>
2022-01-06 12:55:21 +0100

    first commit

---
+++ src/main/java/pl/umg/paw/gamestorepaw/GameStorePawApplication.java
@@ -0,0 +1,13 @@
+package pl.umg.paw.gamestorepaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameStorePawApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameStorePawApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/umg/paw/gamestorepaw/GameStorePawApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.umg.paw.gamestorepaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameStorePawApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
